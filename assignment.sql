8bcda162d2e67a07044817caa426f447f499a02a
profclark <clarkm1@southernct.edu>
2020-12-09 21:15:23 -0500

    Starter files

---
+++ assignment.sql
@@ -0,0 +1,62 @@
+-- Assignment questions/solutions
+
+-- Questions 1 through 5 use only a single table
+
+/*
+Question 1
+Show a list of offices sorted by country, city, and state ascending. Display
+only addressLine1, addressLine2, city, state, and country.
+*/
+
+
+
+
+/*
+Question 2
+List the product lines (only the productLine column) that contain the word "cars".
+*/
+
+
+
+/*
+Question 3
+What payments were greater than $100,000 and
+have a payment date after the year 2003?
+Show all the columns.
+*/
+
+
+
+/*
+Question 4
+How many products does each product line have? Show
+the productLine and the number of products in each productLine. Alias
+the number of products as "num_products".
+*/
+
+
+
+/*
+Question 5
+What is the productCode, productName and buyPrice
+of the most expensive product (highest buyPrice)? Make
+sure to only show one row in your result set.
+*/
+
+-- Questions 6 & 7 involve joins with two tables
+
+/*
+Question 6
+What are the employeeNumbers, lastNames and firstNames of
+employees who work in an office located in Boston?
+*/
+
+
+
+/*
+Question 7
+List the total amount paid by each customer. Show the customer name and
+the total payment amount as "total_payment" and order by customer name asc.
+Include those customers who have never made a payment (will get "null" for total_payment).
+*/
+
